e54094890752d8d1a1235edffaab1c8959010f21
Ko <hattoriface@gmail.com>
2021-01-20 09:23:48 +0900

    Add link id to otherdate & back to menu on the website

--- src/reserve/MainSystemOpenSchool.java
+++ src/reserve/MainSystemOpenSchool.java
@@ -39,14 +39,12 @@ public class MainSystemOpenSchool {
                 ("学校開放（屋内）", "サロンフットボール・フットサル", "札幌市", null, "2021/02/01", "2021/02/27",null);
         //割り当て↑("-施設区分-",    "-利用目的-",      "-札幌市（固定）-","-施設名称[null]","-検索範囲開始日-","-検索範囲終了日-")
         String reserveMonth = "2"; //<-月を指定
-        DateByPlaceMaster shinkotoni = new DateByPlaceMaster("1","2","3","4","5","6"); //
-        DateByPlaceMaster teine = new DateByPlaceMaster("1","2","3","4","5","6");
-        DateByPlaceMaster shinryo = new DateByPlaceMaster("1","2","3","4","5","6");
+        DateByPlaceMaster school01 = new DateByPlaceMaster("1","2","3","4","5","6");    //<--予約したい日付をここに入力
+        DateByPlaceMaster school02 = new DateByPlaceMaster("1","2","3","4","5","6");    //<--予約したい日付をここに入力
+        DateByPlaceMaster school03 = new DateByPlaceMaster("1","2","3","4","5","6");    //<--予約したい日付をここに入力
 
         List<String> places = new ArrayList<>();
-        places.add("新琴似");
-        places.add("手稲山口");
-        places.add("新陵中");
+        Collections.addAll(places, "新琴似","手稲山口","新陵中");  //<-ここに予約したい施設名を追加*138行目以降のschoolの個数と確認
         try {
 
             //ChoromeDriverをセット＆定義
@@ -108,7 +106,10 @@ public class MainSystemOpenSchool {
                     if(yoyaku01.getPlaceName() == null || yoyaku01.getPlaceName().equals("")){
                         int noPlace = places.indexOf("")+1;
                         System.out.println("会場リストの" + noPlace + "番目に会場の指定がありません");
-                        break;
+                    }else if(yoyaku01.getPlaceName() == "屯田北小" || yoyaku01.getPlaceName().equals("屯田北小")) {
+                        yoyaku01.setUsePurpose("トレーニング");
+                    }else{
+                        yoyaku01.setUsePurpose("サロンフットボール・フットサル");
                     }
                     Thread.sleep(2000);//2秒待機
                     driver.navigate().refresh(); //ページをリフレッシュ
@@ -136,20 +137,20 @@ public class MainSystemOpenSchool {
                     element09.click();//検索をクリック
                     //カレンダーが表示されて予約する日にちを指定--------------------------------------------------------------------------
                     List<String> reserveDays = new ArrayList<>();
-                    if (yoyaku01.getPlaceName() == "新琴似") {
-                        Collections.addAll(reserveDays,shinkotoni.getReserveDate01(),shinkotoni.getReserveDate02(),
-                                                        shinkotoni.getReserveDate03(),shinkotoni.getReserveDate04(),
-                                                        shinkotoni.getReserveDate05(),shinkotoni.getReserveDate06());//DateByPlaceクラスからリストに日にちを格納
+                    if (yoyaku01.getPlaceName() == places.get(0)) {
+                        Collections.addAll(reserveDays,school01.getReserveDate01(),school01.getReserveDate02(),
+                                school01.getReserveDate03(),school01.getReserveDate04(),
+                                school01.getReserveDate05(),school01.getReserveDate06());//DateByPlaceクラスからリストに日にちを格納
                     }
-                    if (yoyaku01.getPlaceName() == "手稲山口") {
-                        Collections.addAll(reserveDays,teine.getReserveDate01(),teine.getReserveDate02(),
-                                teine.getReserveDate03(),teine.getReserveDate04(),
-                                teine.getReserveDate05(),teine.getReserveDate06());//DateByPlaceクラスからリストに日にちを格納
+                    if (yoyaku01.getPlaceName() == places.get(1)) {
+                        Collections.addAll(reserveDays,school02.getReserveDate01(),school02.getReserveDate02(),
+                                school02.getReserveDate03(),school02.getReserveDate04(),
+                                school02.getReserveDate05(),school02.getReserveDate06());//DateByPlaceクラスからリストに日にちを格納
                     }
-                    if (yoyaku01.getPlaceName() == "新陵中") {
-                        Collections.addAll(reserveDays,shinryo.getReserveDate01(),shinryo.getReserveDate02(),
-                                shinryo.getReserveDate03(),shinryo.getReserveDate04(),
-                                shinryo.getReserveDate05(),shinryo.getReserveDate06());//DateByPlaceクラスからリストに日にちを格納
+                    if (yoyaku01.getPlaceName() == places.get(2)) {
+                        Collections.addAll(reserveDays,school03.getReserveDate01(),school03.getReserveDate02(),
+                                school03.getReserveDate03(),school03.getReserveDate04(),
+                                school03.getReserveDate05(),school03.getReserveDate06());//DateByPlaceクラスからリストに日にちを格納
                     }
 
                     //ここから繰り返し処理
@@ -168,9 +169,9 @@ public class MainSystemOpenSchool {
                             System.out.println("日にちの指定がありません");
                         }else if (youbi.equals("日曜") || youbi.equals("土曜")) { //土曜日日曜日の時
                             WebElement element11a = driver.findElement(By.id("ctl00_ContentPlaceHolder1_JikantaiSel6"));
-                            element11a.click();//時間帯を指定
-                        } else { //平日の時
-                            WebElement element11b = driver.findElement(By.id("ctl00_ContentPlaceHolder1_JikantaiSel6"));
+                            element11a.click();//時間帯を指定　
+                        } else { //平日の時id:ctl00_ContentPlaceHolder1_JikantaiSel0
+                            WebElement element11b = driver.findElement(By.id("ctl00_ContentPlaceHolder1_JikantaiSel0"));
                             element11b.click();//時間帯を指定
                         }
                         WebElement element12 = driver.findElement(By.id("ctl00_ContentPlaceHolder1_btnShinseiCnf"));
@@ -182,8 +183,12 @@ public class MainSystemOpenSchool {
                         System.out.println(yoyaku01.getPlaceName() + "/" + reserveMonth + "月" + reserveDay + "日をID"
                                 + id + "で予約しました。");
                         //”別の日を指定して予約する”ボタンをクリックする
+                        WebElement otherdate = driver.findElement(By.id("ctl00_ContentPlaceHolder1_btnDateYoyaku"));
+                        otherdate.click();//別日程を検索
                     }
                     //メニューに戻って別の施設の予約をする
+                    WebElement backToMenu = driver.findElement(By.linkText("メニューへ戻る"));
+                    backToMenu.click();//メニューへ戻る
                 }
 
                 Thread.sleep(3000);
--- src/test/Test3.java
+++ src/test/Test3.java
@@ -23,12 +23,12 @@ public class Test3 {
                 null);
         //割り当て↑("-施設区分-",    "-利用目的-",      "-札幌市（固定）-","-施設名称01-","-検索範囲開始日-","-検索範囲終了日-")
         String reserveMonth = "2"; //<-月を指定
-        DateByPlaceMaster shinkotoni = new DateByPlaceMaster("1", "2", "3", "4", "5", "6"); //
-        DateByPlaceMaster teine = new DateByPlaceMaster("11", "12", "13", "14", "15", "16");
-        DateByPlaceMaster shinryo = new DateByPlaceMaster("21", "22", "23", "24", "25", "26");
+        DateByPlaceMaster school01 = new DateByPlaceMaster("1", "2", "3", "4", "5", "6"); //
+        DateByPlaceMaster school02 = new DateByPlaceMaster("11", "12", "13", "14", "15", "16");
+        DateByPlaceMaster school03 = new DateByPlaceMaster("21", "22", "23", "24", "25", "26");
 
         List<String> places = new ArrayList<>();
-        places.add("新琴似");
+        places.add("屯田北小");
         places.add("手稲山口");
         places.add("新陵中");
         //エクセルファイルへアクセスしてID番号を取得する--------------------------------------
@@ -54,28 +54,31 @@ public class Test3 {
                 if(yoyaku01.getPlaceName() == null || yoyaku01.getPlaceName().equals("")){
                     int noPlace = places.indexOf("")+1;
                     System.out.println("会場リストの" + noPlace + "番目に会場の指定がありません");
-                    break;
+                }else if(yoyaku01.getPlaceName() == "屯田北小" || yoyaku01.getPlaceName().equals("屯田北小")) {
+                    yoyaku01.setUsePurpose("トレーニング");
+                }else{
+                    yoyaku01.setUsePurpose("サロンフットボール・フットサル");
                 }
                 List<String> reserveDays = new ArrayList<>();
-                if (yoyaku01.getPlaceName() == "新琴似") {
-                    Collections.addAll(reserveDays,shinkotoni.getReserveDate01(),shinkotoni.getReserveDate02(),
-                                                    shinkotoni.getReserveDate03(),shinkotoni.getReserveDate04(),
-                                                    shinkotoni.getReserveDate05(),shinkotoni.getReserveDate06());//DateByPlaceクラスからリストに日にちを格納
+                if (yoyaku01.getPlaceName() == places.get(0)) {
+                    Collections.addAll(reserveDays,school01.getReserveDate01(),school01.getReserveDate02(),
+                            school01.getReserveDate03(),school01.getReserveDate04(),
+                            school01.getReserveDate05(),school01.getReserveDate06());//DateByPlaceクラスからリストに日にちを格納
                 }
-                if (yoyaku01.getPlaceName() == "手稲山口") {
-                    Collections.addAll(reserveDays,teine.getReserveDate01(),teine.getReserveDate02(),
-                            teine.getReserveDate03(),teine.getReserveDate04(),
-                            teine.getReserveDate05(),teine.getReserveDate06());//DateByPlaceクラスからリストに日にちを格納
+                if (yoyaku01.getPlaceName() == places.get(1)) {
+                    Collections.addAll(reserveDays,school02.getReserveDate01(),school02.getReserveDate02(),
+                            school02.getReserveDate03(),school02.getReserveDate04(),
+                            school02.getReserveDate05(),school02.getReserveDate06());//DateByPlaceクラスからリストに日にちを格納
                 }
-                if (yoyaku01.getPlaceName() == "新陵中") {
-                    Collections.addAll(reserveDays,shinryo.getReserveDate01(),shinryo.getReserveDate02(),
-                            shinryo.getReserveDate03(),shinryo.getReserveDate04(),
-                            shinryo.getReserveDate05(),shinryo.getReserveDate06());//DateByPlaceクラスからリストに日にちを格納
+                if (yoyaku01.getPlaceName() == places.get(2)) {
+                    Collections.addAll(reserveDays,school03.getReserveDate01(),school03.getReserveDate02(),
+                            school03.getReserveDate03(),school03.getReserveDate04(),
+                            school03.getReserveDate05(),school03.getReserveDate06());//DateByPlaceクラスからリストに日にちを格納
                 }
 
                 for (String reserveDay : reserveDays) {
                     yoyaku01.setReserveDate(reserveMonth + "月" + reserveDay + "日");
-                    System.out.println("会場:" + yoyaku01.getPlaceName() + "/" + yoyaku01.getReserveDate());
+                    System.out.println("会場:" + yoyaku01.getPlaceName() + "/目的:"+yoyaku01.getUsePurpose()  + yoyaku01.getReserveDate());
                     ReserveDateController rdc = new ReserveDateController();
 
                     String youbi = rdc.getYoubi("2021", reserveMonth, reserveDay);
